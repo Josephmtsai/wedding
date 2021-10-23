f449ee6018df978851cfa9ac3d4cb30b862c2ee4
joseph <mmx362003@gmail.com>
2021-10-24 01:44:33 +0800

    add to calendar function

--- info/src/views/WeddingInvite.vue
+++ info/src/views/WeddingInvite.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home">
+    <v-container class="grey lighten-5 text-lg-right">
+      <v-row no-gutters>
+        <v-col class="text-left">
+          <v-btn
+            href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=N29jZ3VjaTA5b2czNTQ2ZmdkczcxMDhhbmQgbW14MzYyMDAzQG0&amp;tmsrc=mmx362003%40gmail.com"
+            target="_blank"
+            fab
+            dark
+            small
+            color="pink"
+          >
+            <v-icon>mdi-calendar</v-icon>
+          </v-btn>
+        </v-col></v-row
+      >
+    </v-container>
     <img
       alt="WeddingInfo"
       src="../assets/WEDDING_01.png"
@@ -25,5 +41,10 @@
 export default {
   name: "WeddingInvite",
   components: {},
+  methods: {
+    addToCalendar() {
+      this.msg = "快來看這裡！我是新訊息！";
+    },
+  },
 };
 </script>
