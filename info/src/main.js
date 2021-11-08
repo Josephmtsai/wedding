ded6debd186e0eb47db6764ca659da065d288977
joseph <mmx362003@gmail.com>
2021-11-09 01:39:39 +0800

    adjust

--- info/src/main.js
+++ info/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import router from './router'
+var AddToCalendar = require('vue-add-to-calendar');
 
+Vue.use(AddToCalendar);
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
--- info/src/views/WeddingInvite.vue
+++ info/src/views/WeddingInvite.vue
@@ -3,16 +3,19 @@
     <v-container fluid class="grey lighten-5 text-lg-right">
       <v-row no-gutters>
         <v-col class="text-left">
-          <v-btn
-            href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=N29jZ3VjaTA5b2czNTQ2ZmdkczcxMDhhbmQgbW14MzYyMDAzQG0&amp;tmsrc=mmx362003%40gmail.com"
-            target="_blank"
-            fab
-            dark
-            small
-            color="pink"
+          <add-to-calendar
+            title="Joseph & Ellen 結婚(12點準時入席)"
+            location="典華幸福機構(大直店), 10466台灣台北市中山區植福路8號"
+            :start="new Date(2021, 11, 4, 12)"
+            :end="new Date(2021, 11, 4, 15)"
+            details="似錦廳 5F 聯絡電話:0953966053 "
+            inline-template
           >
-            <v-icon>mdi-calendar</v-icon>
-          </v-btn>
+            <google-calendar id="google-calendar">
+              <i class="v-icon notranslate mdi mdi-calendar theme--dark"></i>
+              Add to Calendar
+            </google-calendar>
+          </add-to-calendar>
         </v-col></v-row
       >
       <v-row no-gutters>
